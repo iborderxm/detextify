63fdfe5da27a0b11204228ca230f00d2667e8bc5
iborder <iborder@139.com>
2026-05-30 22:04:38 +0800

    chore: 修复依赖格式并移动示例代码
    1. 修正requirements.txt中pytest的依赖声明格式
    2. 新增根目录main.py作为默认示例脚本
    3. 删除tests/main.py中的旧示例脚本文件

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from detextify.inpainter import LocalSDInpainter
 from detextify.detextifier import Detextifier
 
 text_detector = TesseractTextDetector("/usr/include/tesseract")
-detextifier = Detextifier(text_detector, LocalSDInpainter(model_path="../mod"))
-detextifier.detextify("../data/1.jpg", "../data/2.jpg")
+detextifier = Detextifier(text_detector, LocalSDInpainter(model_path="./mod"))
+detextifier.detextify("./data/1.jpg", "./data/2.jpg")
